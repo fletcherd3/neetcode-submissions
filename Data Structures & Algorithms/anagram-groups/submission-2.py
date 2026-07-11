2304d15988af486967e39045e9988bd2fdbc03c4
Fletcher Dick <hello@fletcherjd.com>
2026-07-11 14:43:51 +1000

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,41 @@
+"""
+# time to code: 9 mins
+# time to solve: ? mins
+
+# thinking
+- first thought of summing int rep of chars to a number. thought good idea but dont know how to do in python
+- could sort each and compare. slow though ..
+
+# notes
+- 
+
+# add to cheatsheet
+## hash map with default value
+
+```
+from collections import defaultdict
+
+d = defaultdict(lambda: [])
+```
+
+# complexity
+time:  O()
+space: O()
+"""
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # init anagrams (a) hash map (sort → [s])
+        a = defaultdict(lambda: [])
+
+        # iterate over strs (s)
+        for s in strs:
+            # fe(s): sort str and a[sort(s)] += s
+            key = "".join(sorted(s))
+            a[key] += [s]
+
+        # return (join a values)
+        return [v for v in a.values()]
+        
